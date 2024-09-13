19834cbcba1a0e33f6cb9c653e377b6ee42a7e4c
MathyLima <mathy.l.freitas@gmail.com>
2024-09-13 19:09:25 -0300

    volta ao analisador lexico original

--- src/lexical/Token/Token.h
+++ src/lexical/Token/Token.h
@@ -4,65 +4,38 @@
 #include <string>
 #include <iostream>
 
-    enum class TokenType
-    {
-        IDENTIFIER,
-        KEYWORD,
-        NUMBER,
-        FLOAT_NUMBER,
-        ADD_OPERATOR,
-        MULT_OPERATOR,
-        REL_OPERATOR,
-        EQUAL_OPERATOR,
-        DELIMITER,
-        LOGICAL_OPERATOR,
-        ASSIGNMENT,
-        LITERAL,
-        REL_FUNCTION,
-        NONE,
-        PROGRAM,
-        VAR,
-        INTEGER,
-        REAL,
-        BOOLEAN,
-        PROCEDURE,
-        BEGIN,
-        END,
-        IF,
-        THEN,
-        ELSE,
-        WHILE,
-        DO,
-        NOT,
-        COMMA,
-        SEMICOLON,
-        DOT,
-        COLON,
-        PLUS,
-        MINUS,
-        MULTIPLY,
-        DIVIDE,
-        LPAREN,
-        RPAREN,
-        EOF_TOKEN
-    };
+enum class TokenType
+{
+    IDENTIFIER,
+    KEYWORD,
+    NUMBER,
+    FLOAT_NUMBER,
+    ADD_OPERATOR,
+    MULT_OPERATOR,
+    REL_OPERATOR,
+    EQUAL_OPERATOR,
+    DELIMITER,
+    LOGICAL_OPERATOR,
+    ASSIGNMENT,
+    LITERAL,
+    REL_FUNCTION,
+
+    NONE,
+};
 
 class Token
 {
-private:
-    TokenType type;
-    std::string text;
-    std::string row;
+    private:
+        TokenType type;
+        std::string text;
 
-public:
-    Token(TokenType type = TokenType::NONE, const std::string &text = "", const std::string &row );
+    public:
+        Token(TokenType type = TokenType::NONE, const std::string &text = "");
 
-    TokenType getType() const;
-    void setType(TokenType newType);
-    std::string getText() const;
-    std::string getRow() const;
+        TokenType getType() const;
+        std::string getText() const;
 
-    friend std::ostream &operator<<(std::ostream &os, const Token &token);
-};
+        friend std::ostream &operator<<(std::ostream &os, const Token &token);
+    };
 
 #endif
